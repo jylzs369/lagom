d0d896959fe7c30e910fc3170054d18f1fa4033c
Ushio <jylzs369@163.com>
2020-06-10 18:27:18 +0800

    Site updated: 2020-06-10 18:27:17

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+(function () {
+  const scrollTop = function () {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  window.onload = function () {
+    let backTopBtn = document.getElementById('backTop'); // 文章页面，回到顶部按钮点击事件
+
+    backTopBtn && backTopBtn.addEventListener('click', e => {
+      e.preventDefault();
+      scrollTop();
+    });
+  };
+})(window);
+
+},{}]},{},[1]);
